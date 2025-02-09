5dfb562e695b67bb578cb0f7690cd47584a86872
DanielMnslv <176536613+DanielMnslv@users.noreply.github.com>
2025-02-08 23:29:32 -0500

    mysql
    mysql

--- back-end/colegio/colegio/settings.py
+++ back-end/colegio/colegio/settings.py
@@ -99,8 +99,12 @@ WSGI_APPLICATION = 'colegio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'colegio',
+        'USER': 'root',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
